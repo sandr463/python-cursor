d9ef2c904c32680e07f44cb30b8f2247391d1749
sandr463 <stanish2000@gmail.com>
2018-11-13 21:04:25 +0200

    Lecture 1

---
+++ Week7_8_Django/HW_Django/mysite/blog/views.py
@@ -0,0 +1,11 @@
+from .models import Article
+from django.shortcuts import render
+
+
+def index(request):
+    articles = Article.objects.all()
+    return render(request, 'index.html', {'articles': articles})
+
+def detail(request, id_article):
+    article = Article.objects.get(id=id_article)
+    return render(request, 'detail.html', {'article': article})

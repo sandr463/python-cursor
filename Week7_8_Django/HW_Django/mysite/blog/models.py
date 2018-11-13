5afb158f8f44f97bbb8b4a2fdca4a1302581aa83
sandr463 <stanish2000@gmail.com>
2018-11-13 21:09:11 +0200

    Changed files due to PEP8

--- Week7_8_Django/HW_Django/mysite/blog/models.py
+++ Week7_8_Django/HW_Django/mysite/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class Article(models.Model):
     title = models.CharField(max_length=1000, null=True, blank=True)
     description = models.CharField(max_length=1000, null=True, blank=True)
--- Week7_8_Django/HW_Django/mysite/blog/views.py
+++ Week7_8_Django/HW_Django/mysite/blog/views.py
@@ -6,6 +6,7 @@ def index(request):
     articles = Article.objects.all()
     return render(request, 'index.html', {'articles': articles})
 
+
 def detail(request, id_article):
     article = Article.objects.get(id=id_article)
     return render(request, 'detail.html', {'article': article})
